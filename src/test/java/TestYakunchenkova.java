3babdd1779b9c4e805a939f6b68cd25caa883aef
Yakunchenkova <159001685+Yakunchenkova@users.noreply.github.com>
2025-10-15 01:45:46 +0300

    Update TestYakunchenkova.java

--- src/test/java/TestYakunchenkova.java
+++ src/test/java/TestYakunchenkova.java
@@ -3,10 +3,14 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class TestYakunchenkova {
     WebDriver driver;
-    JavascriptExecutor jse = (JavascriptExecutor)driver;
+    JavascriptExecutor jse;
     @BeforeAll
     public  static void installWebDriver(){
         WebDriverManager.chromedriver().setup();
@@ -15,6 +19,7 @@ public class TestYakunchenkova {
     @BeforeEach
     public void openDriver(){
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
     }
 
@@ -33,5 +38,11 @@ public class TestYakunchenkova {
         driver.findElement(By.xpath(("//*[@type = \"password\"]"))).sendKeys("123");
         driver.findElement(By.id("drink2")).click();
         driver.findElement(By.id("drink3")).click();
-    }
-}
+        //WebElement color3 = driver.findElement(By.id("color3"));
+           //     color3.click(); - нужно спуститься ниже по странице.
+        //jse = (JavascriptExecutor)driver;
+       // jse.executeScript("document.getElementById('color3').value = 'Yellow';");
+       // jse.executeScript("window.scrollBy(0,500)",");
+        driver.findElement(By.id("automation")).click();
+        List<WebElement> allUl = driver.findElements(By.tagName("ul"));
+        int number = allUl.size();
